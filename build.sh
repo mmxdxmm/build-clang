1d92d3ef8254f9ea3e7db51a63bbd9bf417ae7f4
mmxdxmm <145817075+mmxdxmm@users.noreply.github.com>
2025-04-13 11:08:02 +0800

    Add files via upload

--- build.sh
+++ build.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
-./build-llvm.py --vendor-string Pdx \
---targets ARM AArch64 \
---projects clang lld \
---build-stage1-only \
---no-update \
--i /root/Toolchain/clang-r498229b
+grep -q "set(LLVM_VERSION_MAJOR 20.0.0)" ./src/llvm-project/llvm/CMakeLists.txt || echo "set(LLVM_VERSION_MAJOR 20.0.0)" >> ./src/llvm-project/llvm/CMakeLists.txt
+./build-llvm.py --vendor-string Pdx --targets ARM AArch64 --projects clang lld --build-stage1-only --no-update -i ./out/clang
 
 
-#./build-binutils.py --targets arm-linux-gnueabi aarch64-linux-gnu -i /root/Toolchain/pdx-clang16
+#./build-binutils.py -B ./src/binutils* --targets arm-linux-gnueabi aarch64-linux-gnu -i ./out/binutls
