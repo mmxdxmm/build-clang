b8f4b22075b5b182c0b309f62a9e20c599418f10
mmxdxmm <145817075+mmxdxmm@users.noreply.github.com>
2025-04-16 02:20:27 +0800

    Add files via upload

--- build.sh
+++ build.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
+#export PATH=$PWD/cmake-4.0.1-linux-aarch64/bin:/lib/llvm-20/bin:$PATH
+
 grep -q "set(LLVM_VERSION_MAJOR 20.0.0)" ./src/llvm-project/llvm/CMakeLists.txt || echo "set(LLVM_VERSION_MAJOR 20.0.0)" >> ./src/llvm-project/llvm/CMakeLists.txt
 ./build-llvm.py --vendor-string="aosp" \
 --targets ARM AArch64 \
---projects clang lld \
+--projects clang lld libc compiler-rt \
 --build-stage1-only \
 --no-update \
--i ./out/aosp-clang-20 \
--D LLVM_PARALLEL_COMPILE_JOBS=7 LLVM_PARALLEL_LINK_JOBS=7 \
+-i ./out/clang \
+-D LLVM_PARALLEL_COMPILE_JOBS=4 LLVM_PARALLEL_LINK_JOBS=2 \
 --build-type Release \
 
 
-#./build-binutils.py -B ./src/binutils* --targets arm-linux-gnueabi aarch64-linux-gnu -i ./out/binutls
+#./build-binutils.py -B ./src/binutils* --targets arm aarch64 -i ./out/binutils
