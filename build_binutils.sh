0337bd80251afcb7b0b32c222b1cc151b0a01e10
李小东 <1658011573@qq.com>
2025-10-15 13:05:37 +0800

    Add files via upload

--- build_binutils.sh
+++ build_binutils.sh
@@ -2,10 +2,10 @@
 -B src/binutils* \
 -b tmp/binutils \
 -t arm aarch64 \
--i /root/binutils \
+-i /home/binutils \
 --show-build-commands
 
 out_dir=$PWD
-cd /root
+cd /home
 zip -r -9 binutils.zip binutils
 cp binutils.zip $out_dir/binutils.zip
